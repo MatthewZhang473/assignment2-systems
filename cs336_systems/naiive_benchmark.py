0ea3d390f09cb019ccf6be9ffed0c745110b9943
matthew <matthew@myrtle.ai>
2026-04-08 17:30:35 +0100

    initial plan benchmark

---
+++ cs336_systems/naiive_benchmark.py
@@ -0,0 +1,16 @@
+from cs336_basics import *
+
+
+# 1. a minimal argparse for model hyperparameters, number of forward/backward passes, run_backward flag,  and # warmup passes
+
+
+# 2. define a BasicTransformer model with random weights
+
+
+# 3. generate random data
+
+
+# 4. helper function for forward pass, backward pass, and timing (timeit)
+
+
+# 5. main function: run w warmup passes, then forward/backward passes, remember to run cuda.synchronize()
